28216631ed19378eecc659ef72a861d64529e98e
Brian Helsel <bhelsel@kumc.edu>
2025-10-14 13:22:29 -0500

    Add Martin Lab Presentation

--- R/helpers.R
+++ R/helpers.R
@@ -1,4 +1,3 @@
-
 #' @title render_presentation
 #' @description Copies presentation to an output directory and opens the HTML presentation in web browser
 #' @param outdir Directory to download the presentation to on your local computer
@@ -17,23 +16,36 @@
 #' @importFrom quarto quarto_render
 #' @importFrom utils browseURL
 
-render_presentation <- function(outdir, name, open_html = TRUE, save_as_pdf = FALSE, ...){
-  if(!dir.exists(outdir)) dir.create(outdir)
+render_presentation <- function(
+  outdir,
+  name,
+  open_html = TRUE,
+  save_as_pdf = FALSE,
+  ...
+) {
+  if (!dir.exists(outdir)) {
+    dir.create(outdir)
+  }
   inputdir <- system.file(file.path("qmd", name), package = "presentationsR")
   file.copy(from = inputdir, to = outdir, recursive = TRUE)
-  qmd_f <- list.files(file.path(outdir, name), pattern = ".qmd$", full.names = TRUE)
+  qmd_f <- list.files(
+    file.path(outdir, name),
+    pattern = ".qmd$",
+    full.names = TRUE
+  )
   quarto::quarto_render(input = qmd_f, ...)
   html_f <- gsub(".qmd$", ".html", qmd_f)
   unlink(file.path(outdir, name, "citations"), recursive = TRUE)
   unlink(file.path(outdir, name, "images"), recursive = TRUE)
   unlink(file.path(outdir, name, "styles"), recursive = TRUE)
   out_html_f <- file.path(outdir, name, basename(html_f))
-  if(open_html) utils::browseURL(html_f)
-  if(save_as_pdf) pagedown::chrome_print(input = html_f, output = gsub(".html$", ".pdf", html_f))
+  if (open_html) {
+    utils::browseURL(html_f)
+  }
+  if (save_as_pdf) {
+    pagedown::chrome_print(
+      input = html_f,
+      output = gsub(".html$", ".pdf", html_f)
+    )
+  }
 }
-
-
-
-
-
-
